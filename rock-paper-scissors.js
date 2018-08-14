cc0928ee375220c6bf45c8c9957c41f9c145a63a
Ben Governali <bgovern93@gmail.com>
2018-08-14 12:16:51 -0400

    Updated playRound to increment player and computer scores accordingly, then edits text content of scoreboard with scores after each round. Removed 'play game' button from index.html. After player or computer has reached 5 points playRound will update results h2 with victor

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,66 +1,46 @@
-//Function for computer to randomly select 'rock', 'paper', or 'scissors'
+let playScore = 0;
+let compScore = 0;
 
 function computerPlay() {
   let compOptions = ['rock', 'paper', 'scissors'];
   computerSelection = compOptions[Math.floor(Math.random()*compOptions.length)];
   return computerSelection;
 }
-  
-//Function for actual play of game. Takes computer's choice and user's choice, then outputs a win or loss
 
 function playRound(playerSelection, computerSelection) {
-    
-  //computerPlay(); //call computerPlay to get a choice from the computer
 
-  //playerSelection = prompt('Select rock, paper, or scissors', ' ');   //Player makes a choice
-  //playerSelection = playerSelection.toLowerCase();
   computerSelection = computerPlay();
-  playerScore = 0
-  computerScore = 0
+  console.log(playerSelection);
+  console.log(computerSelection);
 
-  if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')) { 
-    playScore = document.querySelector('#playScore');
-    playScore += 1;
-    console.log(playScore);
-  }
-  else if (playerSelection == computerSelection) {
-  }
-  else {
-    compScore = document.querySelector('#compScore');
-    compScore += 1;
-    console.log(compScore);
-  }
-}  
-
-/*function playGame() {
-  let playerScore = 0;    //Player Score
-  let computerScore = 0;    //Computer Score
-  let i = 0;
-
-  while (i < 5) {
-    computerPlay();
-    playRound();
-
-    if (results == "win") {
-      playerScore++;
-    } 
-      else if (results == "loss") {
-        computerScore++;
+  if (playScore < 5 && compScore < 5) { 
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')) { 
+      playScore += 1;
+      if (playScore == 5) {
+        result = document.querySelector('#result');
+        result.textContent = "Player Has Won!";
       }
-      else {
-      }
-    i++;
-  }
-  if (playerScore > computerScore) {
-    return "Victory!";
-  } 
-    else if (playerScore == computerScore) {
-      return "Defeat!";  
+    }
+    else if (playerSelection == computerSelection) {
     }
     else {
-      return "Draw!";
+      compScore += 1;
+      if (compScore == 5) {
+        result = document.querySelector('#result');
+        result.textContent = "Computer Has Won!";
+      } 
     }
-}  */
+  }
+  else {
+  }
+  
+  let playerScore = document.querySelector('#playerScore');
+  playerScore.textContent = playScore;
+
+  let computerScore = document.querySelector('#computerScore');
+  computerScore.textContent = compScore;
+
+}  
 
 var rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
